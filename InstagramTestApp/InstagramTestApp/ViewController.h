2451fdd2001a7ecbc6c5c47880857aa14ea8a065
Artem <artem.1@mainpeople.com>
2015-03-06 19:31:39 +0300

    init new project

---
+++ InstagramTestApp/InstagramTestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstagramTestApp
+//
+//  Created by mac on 06.03.15.
+//  Copyright (c) 2015 Artem Porohov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
